2397bfb100d703e2ba922f3225500b631257ae87
gustavo <gustavocrod.unipampa@gmail.com>
2019-06-28 06:31:33 -0300

    last updates done at sex jun 28 06:31:33 -03 2019

--- professor-hulk.sh
+++ professor-hulk.sh
@@ -8,6 +8,24 @@ pingu()
 	echo "Calculating time to $IP... "
 	httping $IP -c $REP > temp_httping.txt
 	echo "$PRINT " `tail -n 1 temp_httping.txt | awk '{print $4}' | cut -d"/" -f2`
+	media=$(tail -n 1 temp.txt | awk '{print $4}' | cut -d "/" -f2)
+	media=${media%.*}
+
+	while read line
+	do
+	    if [[ $line == connected* ]];
+	    then
+	       data=$(echo "$line" | awk '{print $7}' | cut -d"=" -f2)
+	       data=${data%.*}
+	       temp=$(($data - $media)) #xi - media
+	       temp2=$(($temp*$temp)) #(xi-media)^2
+	       acum=$(($acum+$temp2)) #somatorio
+	    fi
+	done < "temp_httping.txt"
+	n=$(($REP-1))
+	variancia=$(($acum/$n)) #somatorio/(n-1)
+	echo "variancia= $variancia"
+	dpad=$(echo "sqrt ( $variancia )" | bc -l) ; echo $dpad
 
 }
 
